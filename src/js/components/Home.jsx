d0f58764ec58f11c16093885a50a7dc6787a6b1d
jeyLopez <lopezgalvisjeanmary@gmail.com>
2025-11-18 02:11:25 +0000

    reproductor

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,96 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useRef, useState } from 'react'
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const audioRef = useRef(null);
+	const [songs, setSongs] = useState([]);
+	const [currentIndex, setCurrentIndex] = useState(null);
+	const [isPlaying, setIsPlaying] = useState(false)
+	useEffect(() => {
+		const cargarCanciones = async () => {
+			try {
+				const respuesta = await fetch('https://playground.4geeks.com/sound/songs');
+				const data = await respuesta.json();
+				setSongs(data.songs);
+				console.log(data);
+				console.log("La canción seleccionada es:", currentIndex)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			} catch (error) {
+				console.error("Error cargando canciones:", error);
+			}
+		};
+
+		cargarCanciones();
+	}, []);
+
+
+	useEffect(() => {
+		if (currentIndex === null) return;
+
+		const song = songs[currentIndex];
+		const fullUrl = "https://playground.4geeks.com" + song.url;
+
+		audioRef.current.src = fullUrl;
+		audioRef.current.play()
+			.then(() => console.log("Reproduciendo:", song.name))
+			.catch(err => console.log("Error al reproducir:", err));
+
+	}, [currentIndex]);
+
+
+	const handlePlayPause = () => {
+  if (!audioRef.current) return;
+
+  if (isPlaying) {
+    audioRef.current.pause();
+    setIsPlaying(false);
+  } else {
+    audioRef.current.play();
+    setIsPlaying(true);
+  }
+};
+const handleNext = () => {
+	if (songs.length === 0) return;
+
+	const nextIndex = (currentIndex + 1) % songs.length;
+
+	setCurrentIndex(nextIndex);
+};
+const handlePrevious = () => {
+	if (songs.length === 0) return;
+
+	const prevIndex = (currentIndex - 1 + songs.length) % songs.length;
+
+	setCurrentIndex(prevIndex);
+};
+
+
+	return (
+		<div className="container-fluid">
+			<div className='container'>
+				<audio ref={audioRef} className='audio-player'></audio>
+			<div className="controls">
+				<button onClick={handlePrevious}> <i className="fa-solid fa-backward icon "></i></button>
+				<button onClick={handlePlayPause}>  {isPlaying ? <i class="fa-solid fa-pause icon"></i> : <i class="fa-solid fa-play icon"></i>}</button>
+				<button onClick={handleNext}><i className="fa-solid fa-forward icon"></i> </button>
+			</div>
+			<ul className='list-s'>
+				{songs.map((song, index) => (
+					<li 
+						key={song.id}
+						onClick={() =>
+							setCurrentIndex(index)}
+							  className={`song-item ${currentIndex === index ? "active" : ""}`}
+					>
+						{index + 1}.{song.name}
+					</li>
+				))}
+			</ul>
+			</div>
 		</div>
 	);
+
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,4 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render( <Home/>)
